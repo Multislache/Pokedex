4c51f4c58b711d379a2e1f22daca8acd90810dbd
Aliinnk <118292890+Aliinnk@users.noreply.github.com>
2022-11-18 13:59:21 +0100

    Add files via upload

---
+++ Pokemon.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 15 17:01:03 2022
+
+@author: atsankova
+"""
+
+class Pokemon:
+    def __init__(self):
+        self.name = Pokemon
+    def Getters:
+    def get-name(self):
+        return self.name
+    def get-nom(self):
+        return self.nom
+    def get-pv(self):
+        return self.pv
+    def __str__:
+    def ascendant:
+    def descendants:
+    def types:
+    def pokemon_by_nom:
+    def table_name:
+    def table_nom:
+    def effectif:
+        
+        
